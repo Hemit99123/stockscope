0a2d44c0243df9d38131c585ac8c29aaf36e6c4e
Hemit <66741904+Hemit99123@users.noreply.github.com>
2021-12-22 12:30:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import datetime as dt
+import streamlit as st
+start = dt.datetime(1700,1,1)
+end = dt.datetime.now()
+st.write(""" 
+# STOCK APP
+
+#### Instructions:
+
+Delete the placeholder "GOOGL" and then search the ticker that you wish.
+""")
+ticker = st.text_input('TICKER', value="GOOGL")
+Stock = yf.download(ticker, start, end)
+st.write(""" 
+Closing price of 
+""" + ticker)
+
+st.line_chart(Stock.Close)
+
+st.write(""" 
+Volume price of
+""" + ticker)
+st.line_chart(Stock.Volume)
