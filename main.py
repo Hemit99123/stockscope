c656f9e5e3a6206a3e43a86dfa97185c113bf959
Hemit <66741904+Hemit99123@users.noreply.github.com>
2021-12-23 10:06:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,15 +40,15 @@ Other stocks you can buy other then
 """ + ticker.upper())
 recommendation = tickerData.recommendations
 
-if recommendation == ' ':
+if recommendation == False:
   st.write("""
   None
   """)
-if dividends == ' ':
+if dividends == False:
   st.write("""
   None
   """)
-if holder == ' ':
+if holder == False:
   st.write("""
   None
   """)
